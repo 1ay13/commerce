bc6954157a1ef7a163deeba7a48dc70d13768d0c
1ay13 <jayhthakar1997@gmail.com>
2024-03-10 18:27:19 -0400

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
+
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+  rules: {
+    'no-console': 'off',
   },
 }
